4316dc3919566cbbc58dc7a2c03f0e22e071c139
EMILIA <69180101+emi490@users.noreply.github.com>
2021-02-02 18:32:38 +0000

    create function to generate 5 random hues from parameters

--- gen.js
+++ gen.js
@@ -15,10 +15,38 @@ function colorsFromHex(hex) {
         h *= 60;
         s = d / (1 - Math.abs(2 * l - 1));
     }
-    const HSL = [h, s * 100, l * 100].map(x => Math.round(x));
+    const HSL = [h, s * 100, l * 100].map(x => {return x < 0 ? x + 360 : x}).map(x => {return Math.round(x)});
     return {
         'hex': hex,
         'rgb': RGB,
         'hsl': HSL
     }
+}
+function generate(colorCodes, method) {
+    const METHODS = {
+        monochrome: [0],
+        analogous: [-60, 0, 60],
+        complementary: [0, 180],
+        split_complementary: [0, 150, 210],
+        triangle: [0, 120, 240],
+        tetradic: [0, 60, 180, 240],
+        tetradic_square: [0, 90, 180, 270]
+    };
+    const BASE = colorCodes.hsl[0];
+    const KEYS = Object.keys(METHODS);
+    function randomIndex(a) {
+        return Math.floor(Math.random() * a.length)
+    }
+    function hues(b, m) {
+        const gHues = [];
+        while (gHues.length < 5) {
+            if (m === 'analogous') {
+                gHues.push(BASE + (Math.floor(Math.random() * (METHODS[m][2] - METHODS[m][0]) + METHODS[m][0])));
+            } else {
+                gHues.push(BASE + METHODS[m][randomIndex(METHODS[m])]);
+            }
+        }
+        return gHues.map(x => {return x < 0 ? x + 360 : x >= 360 ? x - 360 : x}) + m + BASE;
+    }
+    return method === 'auto' ? hues(BASE, KEYS[randomIndex(KEYS)]) : hues(BASE, method);
 }
